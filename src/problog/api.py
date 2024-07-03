365c4507ffdb5ef95fb9672b2c8e814e0e29c1df
hobs <git@totalgood.com>
2024-07-03 17:17:29 -0500

    api.py

--- src/problog/api.py
+++ src/problog/api.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 LLM_API_KEY = os.environ.get("OPEN_ROUTER_KEY")
-LLM_API_FQDN, LLM_API_PATH = "api.openai.com"
+LLM_API_FQDN, LLM_API_PATH = "api.openai.com", 'v1'
 # LLM_FQDN = "api.openai.com"
 # LLM_FQDN = "api.openai.com"
 print(f'key: {LLM_API_KEY[:3]}...{LLM_API_KEY[-2:]}')
@@ -17,7 +17,7 @@ def get_client(domain=LLM_API_FQDN, path=LLM_API_PATH):  # "api.together.xyz"):
     )
 
 
-def get_aclient(domain="api.openai.com", path='v1'):  # "api.together.xyz"):
+def get_aclient(domain=LLM_API_FQDN, path=LLM_API_PATH):  # "api.together.xyz"):
     return AsyncOpenAI(
         api_key=LLM_API_KEY,
         base_url=f"https://{domain}/{path.strip('/')}/",
