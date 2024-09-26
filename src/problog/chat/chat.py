d8c4aebd65b75369201ce71db4a3746dd555c826
hobs <github@totalgood.com>
2024-09-26 14:07:43 -0700

    chat.py formatted by hobs?

--- src/problog/chat/chat.py
+++ src/problog/chat/chat.py
@@ -14,29 +14,30 @@ load_dotenv()
 # ========================================= CHAT ===================================>
 # ==================================================================================>
 
+
 def chat(
-        messages, 
-        provider='openai', 
-        model='gpt-3.5-turbo', 
-        systemPrompt="You are an AI assistant", 
-        maxTokens=1024
-    ):
+    messages,
+    provider='openai',
+    model='gpt-3.5-turbo',
+    systemPrompt="You are an AI assistant",
+    maxTokens=1024
+):
     client = get_client(provider)
     response = client.chat.completions.create(
         messages=[{"role": "system", "content": systemPrompt}, *messages],
         model=model, max_tokens=maxTokens, stream=True, logprobs=True,
     )
-    
+
     colors = get_colors()
-    colored_text = "" 
-    pure_text = ""   
+    colored_text = ""
+    pure_text = ""
 
     for chunk in response:
         choice = chunk.choices[0]
         content = choice.delta.content
         log_probs = choice.logprobs
         if content and log_probs:
-            pure_text += content 
+            pure_text += content
 
             log_prob = log_probs.content[0].logprob
             linear_prob = np.round(np.exp(log_prob) * 100, 2)
@@ -50,5 +51,5 @@ def chat(
     print("FULL TEXT RESPONSE (colored):", colored_text)
     print("---------------------------------------------------------------")
     print("FULL TEXT RESPONSE (pure):", pure_text)
-    
+
     return {"colorContent": colored_text, "content": pure_text}
