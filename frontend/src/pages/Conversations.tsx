bc1b612e0606b9282e4b8146667474941e245da9
Fayziev Fazliddin <fazrez4515@gmail.com>
2024-09-26 16:49:17 -0400

    backend api

--- frontend/src/pages/Conversations.tsx
+++ frontend/src/pages/Conversations.tsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Box, Typography, List, ListItem, ListItemText, CircularProgress } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-// const BASE_URL = "http://172.235.53.175:5000";
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "http://172.235.53.175:5000";
 
 type Conversation = {
     id: string;
