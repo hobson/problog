88420f3f6e8ea9e71305ba85fd70fb631afd27d2
Fayziev Fazliddin <fazrez4515@gmail.com>
2024-09-30 09:51:08 -0400

    color changes in backed, remove reset conversation

--- src/problog/functions/logprob.py
+++ src/problog/functions/logprob.py
@@ -1,17 +1,17 @@
-import tastymap as tm
 from dotenv import load_dotenv
 
 load_dotenv()
-COLORMAP = "viridis_r"
-NUM_COLORS = 11 
 
+custom_colors = ['#FF7074', '#FFA756', '#7ad151', '#43bf71',
+    '#22a884', '#21908c', '#2a788e', '#345f8d',
+    '#414487', '#482575', '#440154']
 
 def get_colors():
-    tmap = tm.cook_tmap(COLORMAP, reverse=False, num_colors=NUM_COLORS, name='Token probability', value=1)
-    colors = tmap.to_model("hex")
-    return colors
+    return custom_colors
 
-# Output of Colors: 
-# ['#fde725','#bcdf27','#7ad151','#43bf71',
-# '#22a884','#21908c','#2a788e','#345f8d',
-# '#414487','#482575','#440154']
+colors = get_colors()
+
+# Output of Colors:
+# ['#FF7074', '#FFA756', '#7ad151', '#43bf71',
+#     '#22a884', '#21908c', '#2a788e', '#345f8d',
+#     '#414487', '#482575', '#440154']
